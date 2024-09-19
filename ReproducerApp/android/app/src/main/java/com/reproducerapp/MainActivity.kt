73952fb4c45824a8eff3afe0e258b6f0e483f2dc
Kiruba <kirubashankar@ghoshak.com>
2024-09-19 22:59:42 +0530

    init

--- ReproducerApp/android/app/src/main/java/com/reproducerapp/MainActivity.kt
+++ ReproducerApp/android/app/src/main/java/com/reproducerapp/MainActivity.kt
@@ -1,22 +1,38 @@
 package com.reproducerapp
 
-import com.facebook.react.ReactActivity
-import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.ContentResolver
+import android.graphics.drawable.Drawable
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import android.media.AudioAttributes
+import android.net.Uri
+import android.os.Build
+import android.widget.LinearLayout
+import android.os.Bundle
+import org.devio.rn.splashscreen.SplashScreen
+import com.reactnativenavigation.NavigationActivity
 
-class MainActivity : ReactActivity() {
-
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "ReproducerApp"
-
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+class MainActivity : NavigationActivity() {
+    @Override
+    protected fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel("long_notification_sound", "Notification", NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.setShowBadge(true)
+            notificationChannel.setDescription("")
+            val att: AudioAttributes = Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                    .build()
+            notificationChannel.setSound(Uri.parse((ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getPackageName()).toString() + "/raw/iphoneringtone4"), att)
+            notificationChannel.enableVibration(true)
+            notificationChannel.setVibrationPattern(longArrayOf(400, 400))
+            notificationChannel.setLockscreenVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            val manager: NotificationManager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(notificationChannel)
+        }
+        SplashScreen.show(this)
+        super.onCreate(savedInstanceState)
+    }
 }
